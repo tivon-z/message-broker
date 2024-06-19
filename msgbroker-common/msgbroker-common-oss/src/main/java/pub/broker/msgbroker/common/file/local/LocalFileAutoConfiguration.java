f30248d2bfc5b1fd78cf7c2765f8f8cd02e856d8
tivon <tivon.zhao@gmail.com>
2024-06-19 11:57:46 +0800

    common oss

---
+++ msgbroker-common/msgbroker-common-oss/src/main/java/pub/broker/msgbroker/common/file/local/LocalFileAutoConfiguration.java
@@ -0,0 +1,45 @@
+/*
+ *    Copyright (c) 2018-2025, lengleng All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * Neither the name of the PIG4CLOUD.com developer nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * Author: lengleng (wangiegie@gmail.com)
+ */
+
+package pub.broker.msgbroker.common.file.local;
+
+import pub.broker.msgbroker.common.file.core.FileProperties;
+import pub.broker.msgbroker.common.file.core.FileTemplate;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * aws 自动配置类
+ *
+ * @author broker.pub
+ * @author 858695266
+ */
+@AllArgsConstructor
+public class LocalFileAutoConfiguration {
+
+	private final FileProperties properties;
+
+	@Bean
+	@ConditionalOnMissingBean(LocalFileTemplate.class)
+	@ConditionalOnProperty(name = "file.local.enable", havingValue = "true", matchIfMissing = true)
+	public FileTemplate localFileTemplate() {
+		return new LocalFileTemplate(properties);
+	}
+
+}
